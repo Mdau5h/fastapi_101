80a6734266ca62b6cd375cb8445138156f84f202
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2023-01-04 23:04:13 +0300

    add model and mock db for get_notes method

---
+++ database/models.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+
+class Note(BaseModel):
+    id: int
+    title: str = Field(..., max_length=50)
+    content: str = Field(...)
--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI, HTTPException
+from typing import List
 import uvicorn
+from database.models import Note
+
+db:List[Note] = [
+    Note(
+        id=0,
+        title="aaa",
+        content="bbb"
+    ),
+    Note(
+        id=1,
+        title="ccc",
+        content="ddd"
+    )
+]
 
 app = FastAPI()
 
+@app.get("/")
+async def home():
+    return {"message": "Hello, world!"}
 
+
+@app.get("/api/v1/notes")
+async def get_notes():
+    return db
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
