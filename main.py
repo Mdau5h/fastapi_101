20b38aefcb465858b67605d749c7975b4d3be754
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2023-01-04 23:37:51 +0300

    add post and delete methods

--- main.py
+++ main.py
@@ -22,10 +22,22 @@ app = FastAPI()
 async def home():
     return {"message": "Hello, world!"}
 
-
 @app.get("/api/v1/notes")
 async def get_notes():
     return db
+
+@app.post("/api/v1/notes", status_code=201)
+async def create_note(note: Note):
+    db.append(note)
+    return note
+
+@app.delete("/api/v1/notes/{note_id}", status_code=204)
+async def delete_note(note_id: int):
+    for note in db:
+        if note.id == note_id:
+            db.remove(note)
+            return
+    raise HTTPException(status_code=404, detail=f"Note with id == '{note_id}' does not exist!")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
