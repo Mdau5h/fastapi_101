17609361d1926d7cba34623d6c1d23afe4402d53
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2023-01-05 16:09:51 +0300

    add logger

--- app/config.py
+++ app/config.py
@@ -1,12 +1,19 @@
 import dotenv
+import os
 
-from pydantic import BaseSettings, Field
-
+from pydantic import BaseSettings, Field, validator
 
 dotenv.load_dotenv()
 
-
 class Config(BaseSettings):
     APP_NAME: str = Field('mdau5h-notes', const=True)
+
+    LOG_FILE: str
+
+    @validator('LOG_FILE')
+    def check_logfile_if_exists(cls, value):
+        if not os.path.isfile(value):
+            raise ValueError(f'Logfile {value!r} does not exists')
+        return value
 
 config = Config()
