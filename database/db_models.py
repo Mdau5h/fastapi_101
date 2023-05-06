3b6bd54c872450eaec229985005db1412bde66f3
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2023-05-06 20:19:54 +0300

    refactor models for new crud

--- database/db_models.py
+++ database/db_models.py
@@ -1,9 +1,22 @@
-from sqlalchemy import Column, BigInteger, String
-from sqlalchemy.orm import declarative_base
+import datetime
+from sqlalchemy import Column, BigInteger, String, Boolean, DateTime
+from sqlalchemy.orm import declarative_base, declarative_mixin
+
 
 Base = declarative_base()
 
-class Note(Base):
+@declarative_mixin
+class BaseMixin:
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    deleted = Column(Boolean, default=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_at = Column(
+        DateTime, nullable=False,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.utcnow
+    )
+
+class Note(BaseMixin, Base):
     __tablename__ = 'note'
 
     id = Column(BigInteger, autoincrement=True, primary_key=True)
